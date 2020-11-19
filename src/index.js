7bc2151d9c95fee0944fa4421120a85b044a55c4
Marco Barbosa <marco.meireles.b@gmail.com>
2020-11-19 19:07:49 -0300

    Corringindo erros do lint nos arquivos que ja vieram do repositorio para que o meu PR seja avaliado

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ const net = require('net');
 
 const { getLocationInfos } = require('./location');
 
-const getHeaderValue = (data, header) => {
-  const headerData = data
-    .split('\r\n')
-    .find((chunk) => chunk.startsWith(header));
+// const getHeaderValue = (data, header) => {
+//   const headerData = data
+//     .split('\r\n')
+//     .find((chunk) => chunk.startsWith(header));
 
-  return headerData.split(': ').pop();
-};
+//   return headerData.split(': ').pop();
+// };
 
 const startOfResponse = null;
 
@@ -17,6 +17,7 @@ const endOfResponse = null;
 const server = net.createServer((socket) => {
   socket.on('data', (data) => {
     const clientIP = null;
+    console.log(data);
 
     getLocationInfos(clientIP, (locationData) => {
       socket.write(startOfResponse);
@@ -26,6 +27,7 @@ const server = net.createServer((socket) => {
       socket.write('<iframe src="https://giphy.com/embed/l3q2zVr6cu95nF6O4" width="480" height="236" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>');
       socket.write('</body></html>');
       socket.write(endOfResponse);
+      console.log(locationData);
     });
   });
 });
