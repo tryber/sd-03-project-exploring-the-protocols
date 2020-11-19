e7e9e30a17cbd1bd55109e6409b097699cdc82b1
Ana Carolina Gomes <gomes.anac03@gmail.com>
2020-11-19 20:06:34 -0300

    Alguns requisitos

--- src/index.js
+++ src/index.js
@@ -2,28 +2,36 @@ const net = require('net');
 
 const { getLocationInfos } = require('./location');
 
-// const getHeaderValue = (data, header) => {
-//   const headerData = data
-//     .split('\r\n')
-//     .find((chunk) => chunk.startsWith(header));
+const getHeaderValue = (data, header) => {
+  const headerData = data
+    .split('\r\n')
+    .find((chunk) => chunk.startsWith(header));
+  return headerData.split(': ').pop();
+};
 
-//   return headerData.split(': ').pop();
-// };
+const startOfResponse = `${[
+  'HTTP/1.1 200 OK',
+  'Content-Type: text/html; charset=UTF-8',
+].join('\r\n')}\r\n\r\n`;
 
-const startOfResponse = null;
-
-const endOfResponse = null;
+const endOfResponse = '\r\n\r\n';
 
 const server = net.createServer((socket) => {
-  socket.on('data', (_data) => {
-    const clientIP = null;
+  socket.on('data', (data) => {
+    console.log('data', data.toString());
+    const clientIP = getHeaderValue(data.toString(), 'X-Forwarded-For');
 
     getLocationInfos(clientIP, (_locationData) => {
+      console.log('start', startOfResponse);
       socket.write(startOfResponse);
-      socket.write('<html><head><meta http-equiv="content-type" content="text/html;charset=utf-8">');
+      socket.write(
+        '<html><head><meta http-equiv="content-type" content="text/html;charset=utf-8">',
+      );
       socket.write('<title>Trybe 🚀</title></head><body>');
       socket.write('<H1>Explorando os Protocolos 🧐🔎</H1>');
-      socket.write('<iframe src="https://giphy.com/embed/l3q2zVr6cu95nF6O4" width="480" height="236" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>');
+      socket.write(
+        '<iframe src="https://giphy.com/embed/l3q2zVr6cu95nF6O4" width="480" height="236" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>',
+      );
       socket.write('</body></html>');
       socket.write(endOfResponse);
     });
