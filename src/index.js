695f42a69493ee9b0f7b8cdf920408809bbcb9f4
Marco Barbosa <marco.meireles.b@gmail.com>
2020-11-20 18:56:12 -0300

    Requisito 10 configurado

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ const endOfResponse = '\r\n\r\n';
 
 const server = net.createServer((socket) => {
   socket.on('data', (data) => {
-    const clientIP = null;
+    const clientIP = getHeaderValue(data.toString(), 'X-Forwarded-For');
     console.log(data);
 
     getLocationInfos(clientIP, (locationData) => {
@@ -25,6 +25,7 @@ const server = net.createServer((socket) => {
       socket.write('<title>Trybe 🚀</title></head><body>');
       socket.write('<H1>Explorando os Protocolos 🧐🔎</H1>');
       socket.write('<iframe src="https://giphy.com/embed/l3q2zVr6cu95nF6O4" width="480" height="236" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>');
+      socket.write(`<p data-testid="ip">IP do cliente: ${locationData.ip}</p>`);
       socket.write('</body></html>');
       socket.write(endOfResponse);
       console.log(locationData);
