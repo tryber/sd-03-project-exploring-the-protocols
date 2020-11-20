a02df19ea76734477826bc4f86616f9429d478f7
Ana Carolina Gomes <gomes.anac03@gmail.com>
2020-11-20 17:32:41 -0300

    Projeto funcionando

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const net = require('net');
+const os = require('os');
 
 const { getLocationInfos } = require('./location');
 
@@ -15,11 +16,13 @@ const endOfResponse = '\r\n\r\n';
 
 const server = net.createServer((socket) => {
   socket.on('data', (data) => {
-    console.log('data', data.toString());
     const clientIP = getHeaderValue(data.toString(), 'X-Forwarded-For');
+    const userAgent = getHeaderValue(data.toString(), 'User-Agent');
+    const arch = os.arch();
+    const cpus = os.cpus();
+    const memory = os.freemem();
 
-    getLocationInfos(clientIP, (_locationData) => {
-      console.log('start', startOfResponse);
+    getLocationInfos(clientIP, (locationData) => {
       socket.write(startOfResponse);
       socket.write(
         '<html><head><meta http-equiv="content-type" content="text/html;charset=utf-8">',
@@ -29,8 +32,24 @@ const server = net.createServer((socket) => {
       socket.write(
         '<iframe src="https://giphy.com/embed/l3q2zVr6cu95nF6O4" width="480" height="236" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>',
       );
+      socket.write('<h3>Informações do Cliente:</h3>');
+      socket.write(`<p data-testid="ip">IP do cliente: ${locationData.ip}</p>`);
+      socket.write(`<p data-testid="city">Cidade: ${locationData.city}</p>`);
+      socket.write(`<p data-testid="postal_code">Código postal: ${locationData.postal_code}</p>`);
+      socket.write(`<p data-testid="region">Região: ${locationData.region}</p>`);
+      socket.write(`<p data-testid="country">País: ${locationData.country}</p>`);
+      socket.write(`<p data-testid="company">Companinha: ${locationData.company}</p>`);
+      socket.write(`<p data-testid="divice">Dispositivo: ${userAgent}</p>`);
+      socket.write('<h3>Informações do Servidor:</h3>');
+      socket.write(`<p data-testid="arch">Arch: ${arch}</p>`);
+      socket.write('<p>CPUS:<p/>');
+      socket.write('<ul>');
+      cpus.forEach(({ model, speed }) => socket.write(`<li data-testid="cpu">Modelo: ${model} - Velocidade: ${speed}</li>`));
+      socket.write('</ul>');
+      socket.write(`<p data-testid="memory">Memória: ${memory}</p>`);
       socket.write('</body></html>');
       socket.write(endOfResponse);
+      socket.end();
     });
   });
 });
