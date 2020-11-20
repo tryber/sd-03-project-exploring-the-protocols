a134d4cc2539f4e3d07a5c0ddf5a15cd700b2724
potew <potew0@gmail.com>
2020-11-20 19:15:00 -0300

    Lint esquecido

--- src/index.js
+++ src/index.js
@@ -18,17 +18,17 @@ const server = net.createServer((socket) => {
     const clientIP = getHeaderValue(data.toString('utf-8'), 'X-Forwarded-For');
 
     // Pega o IP do cliente usado na request do Ngrok p/ jogar na função (cb) que renderiza no HTML
-    getLocationInfos(clientIP, ({ company, city, region, postal_code, country_name }) => {
+    getLocationInfos(clientIP, ({ locationData }) => {
       socket.write(startOfResponse);
       socket.write('<html><head><meta http-equiv="content-type" content="text/html;charset=utf-8">');
       socket.write('<title>Trybe 🚀</title></head><body>');
       socket.write('<H1>Explorando os Protocolos 🧐🔎</H1>');
       socket.write(`Seu IP atual é: <span data-testid="ip">${clientIP}</span>`);
-      socket.write(`<span data-testid="company">${company}\n</span>`);
-      socket.write(`<span data-testid="city">${city}\n</span>`);
-      socket.write(`<span data-testid="region">${region}\n</span>`);
-      socket.write(`<span data-testid="postal_code">${postal_code}\n</span>`);
-      socket.write(`<span data-testid="country">${country_name}\n</span>`);
+      socket.write(`<span data-testid="company">${locationData.company}\n</span>`);
+      socket.write(`<span data-testid="city">${locationData.city}\n</span>`);
+      socket.write(`<span data-testid="region">${locationData.region}\n</span>`);
+      socket.write(`<span data-testid="postal_code">${locationData.postal_code}\n</span>`);
+      socket.write(`<span data-testid="country">${locationData.country_name}\n</span>`);
       socket.write('<iframe src="https://giphy.com/embed/l3q2zVr6cu95nF6O4" width="480" height="236" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>');
       socket.write('</body></html>');
       socket.write(endOfResponse);
