1fe2f1db92d5f8110b41da43ea58969aff093ea7
fabemiliano <fab.emiliano@gmail.com>
2020-11-20 10:34:31 -0300

    trying requisite 10

--- src/index.js
+++ src/index.js
@@ -27,6 +27,7 @@ const server = net.createServer((socket) => {
       socket.write('<title>Trybe 🚀</title></head><body>');
       socket.write('<H1>Explorando os Protocolos 🧐🔎</H1>');
       socket.write('<iframe src="https://giphy.com/embed/l3q2zVr6cu95nF6O4" width="480" height="236" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>');
+      socket.write(`<p data-testid="ip">${clientIP}</p>`);
       socket.write('</body></html>');
       socket.write(endOfResponse);
     });
