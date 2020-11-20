68ee6b2fb579c25074630f9b0a3621658f65392c
Marco Barbosa <marco.meireles.b@gmail.com>
2020-11-20 18:35:06 -0300

    Requisitos 5 e 6 completados

--- src/index.js
+++ src/index.js
@@ -10,9 +10,9 @@ const { getLocationInfos } = require('./location');
 //   return headerData.split(': ').pop();
 // };
 
-const startOfResponse = null;
+const startOfResponse = 'HTTP/1.1 200 OK\r\n Content-Type: text/html; charset=UTF-8\r\n\r';
 
-const endOfResponse = null;
+const endOfResponse = '\r\n\r\n';
 
 const server = net.createServer((socket) => {
   socket.on('data', (data) => {
