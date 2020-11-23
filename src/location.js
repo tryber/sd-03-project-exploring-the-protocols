100a38150c53ad2884b05b55b31bf71a719c183d
Cardoso <lsc.cardosoo@gmail.com>
2020-11-23 15:27:36 -0300

    Enviar o ip no request location client

--- src/location.js
+++ src/location.js
@@ -1,19 +1,24 @@
 const https = require('https');
 
 const options = {
-  //
+  hostname: 'iplocation.com',
+  port: 443,
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/x-www-form-urlencoded',
+  },
 };
 
 const getLocationInfos = (clientIP, cb) => {
   const req = https.request(options, (res) => {
     res.on('data', (locationDataRaw) => {
       const locationData = JSON.parse(locationDataRaw.toString());
-
       console.log('Location data:');
       console.log(locationData);
 
       cb(locationData);
     });
+    // res.write(`ip=${clientIP}`);
   });
 
   req.on('error', (e) => {
@@ -21,6 +26,7 @@ const getLocationInfos = (clientIP, cb) => {
   });
 
   // TO DO: Enviar mensagem (IP) ao server
+  req.write(`ip=${clientIP}`);
 
   req.end();
 };
