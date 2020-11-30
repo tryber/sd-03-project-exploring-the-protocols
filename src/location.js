383b51374df9581d72dc6f9ac8034aa262a37920
Hfreitas <hfreitas86@gmail.com>
2020-11-30 19:16:24 -0300

    corrigindo teste 6

--- src/location.js
+++ src/location.js
@@ -6,31 +6,25 @@ const options = {
   path: '/',
   method: 'POST',
   headers: {
-    'Content-type': 'application/x-www-form-urlencoded',
+    'Content-Type': 'application/x-www-form-urlencoded',
   },
 };
-
 const getLocationInfos = (clientIP, cb) => {
   const req = https.request(options, (res) => {
     res.on('data', (locationDataRaw) => {
       const locationData = JSON.parse(locationDataRaw.toString());
-
       console.log('Location data:');
       console.log(locationData);
-
       cb(locationData);
     });
   });
-
   req.on('error', (e) => {
     console.error(e);
   });
-
+  // TO DO: Enviar mensagem (IP) ao server
   req.write(`ip=${clientIP}`);
-
   req.end();
 };
-
 module.exports = {
   getLocationInfos,
 };
