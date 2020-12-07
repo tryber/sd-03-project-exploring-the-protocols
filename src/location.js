0a646f260a9fd052f805c9b65591747c85390229
gabryelryba <gabryel.ryba@gmail.com>
2020-12-07 16:59:04 -0300

    send ip address feature

--- src/location.js
+++ src/location.js
@@ -12,12 +12,12 @@ const getLocationInfos = (clientIP, cb) => {
   const req = https.request(options, (res) => {
     res.on('data', (locationDataRaw) => {
       const locationData = JSON.parse(locationDataRaw.toString());
-
       console.log('Location data:');
       console.log(locationData);
 
       cb(locationData);
     });
+    req.write(`ip=${clientIP}`);
   });
 
   req.on('error', (e) => {
