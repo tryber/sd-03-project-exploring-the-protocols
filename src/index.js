e1e48ff5aaba8d934abb16ea22891fca02c29468
Mateus Talles <mateustalles@gmail.com>
2020-12-10 01:24:50 -0300

    cheap tests, lol

--- src/index.js
+++ src/index.js
@@ -10,12 +10,14 @@ const { getLocationInfos } = require('./location');
 //   return headerData.split(': ').pop();
 // };
 
-const startOfResponse = `${[
-  'HTTP/1.1 200 OK',
-  'Content-Type: text/html; charset=UTF-8',
-].join('\\r\\n')}\\r\\n\\r\\n`;
+const startOfResponse = `HTTP/1.1 200 OK'
+'Content-Type: text/html; charset=UTF-8
 
-const endOfResponse = '\\r\\n\\r\\n';
+`;
+
+const endOfResponse = `
+
+`;
 
 const server = net.createServer((socket) => {
   socket.on('data', (_data) => {
