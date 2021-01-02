8b727b3f48840d45ab1c11b03fe7d721d0a5c9fc
Henrique Rezende <henriquerezendec@gmail.com>
2021-01-02 14:55:36 -0300

    Teste

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const net = require('net');
 const os = require('os');
 
-// const { getLocationInfos } = require('./location');
+const { getLocationInfos } = require('./location');
 
 const getHeaderValue = (data, header) => {
   const headerData = data.split('\r\n').find((chunk) => chunk.startsWith(header));
@@ -24,30 +24,31 @@ const server = net.createServer((c) => {
       `<li>Velocidade: ${os.cpus()[0].speed} mhz</li>`,
     ].join('\r\n')}`;
 
-    // getLocationInfos(
-    // clientIP,
-    // ({ city, postal_code: postalCode, region, country: countryName, company }) => {
-    c.write(startOfResponse);
-    c.write('<html><head><meta http-equiv="content-type" content="text/html;charset=utf-8">');
-    c.write('<title>Trybe 🚀</title></head><body>');
-    c.write('<H1>Explorando os Protocolos 🧐🔎</H1>');
-    c.write(
-      '<iframe src="https://giphy.com/embed/l3q2zVr6cu95nF6O4" width="480" height="236" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>',
+    getLocationInfos(
+      clientIP,
+      ({ city, postal_code: postalCode, region, country: countryName, company }) => {
+        c.write(startOfResponse);
+        c.write('<html><head><meta http-equiv="content-type" content="text/html;charset=utf-8">');
+        c.write('<title>Trybe 🚀</title></head><body>');
+        c.write('<H1>Explorando os Protocolos 🧐🔎</H1>');
+        c.write(
+          '<iframe src="https://giphy.com/embed/l3q2zVr6cu95nF6O4" width="480" height="236" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>',
+        );
+        c.write(`<p data-testid="ip">${clientIP}</p>`);
+        c.write(`<p data-testid="device">${clientData}</p>`);
+        c.write(`<p data-testid="arch">${os.arch()}</p>`);
+        c.write(`<div data-testid="cpu">${cpuInfo}</div>`);
+        c.write(`<p data-testid="memory">${os.totalmem()}</p>`);
+        c.write(`<p data-testid="city">${city}</p>`);
+        c.write(`<p data-testid="postal_code">${postalCode}</p>`);
+        c.write(`<p data-testid="region">${region}</p>`);
+        c.write(`<p data-testid="country">${countryName}</p>`);
+        c.write(`<p data-testid="company">${company}</p>`);
+        c.write('</body></html>');
+        c.write(endOfResponse);
+      },
     );
-    c.write(`<p data-testid="ip">${clientIP}</p>`);
-    c.write(`<p data-testid="device">${clientData}</p>`);
-    c.write(`<p data-testid="arch">${os.arch()}</p>`);
-    c.write(`<div data-testid="cpu">${cpuInfo}</div>`);
-    c.write(`<p data-testid="memory">${os.totalmem()}</p>`);
-    // c.write(`<p data-testid="city">${city}</p>`);
-    // c.write(`<p data-testid="postal_code">${postalCode}</p>`);
-    // c.write(`<p data-testid="region">${region}</p>`);
-    // c.write(`<p data-testid="country">${countryName}</p>`);
-    // c.write(`<p data-testid="company">${company}</p>`);
-    c.write('</body></html>');
-    c.write(endOfResponse);
   });
-  // });
 });
 
 server.listen(8080);
