6bec77f9338eeec82806f3d72f59949b2f71073d
wendersonbelko <contato@wendersonbelko.com>
2021-01-03 16:17:10 -0300

    aaaaaaaaa

--- src/index.js
+++ src/index.js
@@ -18,26 +18,33 @@ const endOfResponse = '\r\n\r\n';
 const server = net.createServer((socket) => {
   socket.on('data', (data) => {
     const clientIP = getHeaderValue(data.toString(), 'X-Forwarded-For');
-    const device = getHeaderValue(data.toString(), 'User-Agent');
+    const clientData = getHeaderValue(data.toString(), 'User-Agent');
+    const cpuInfo = `${[
+      'CPU Info:',
+      `<li>N° de cores: ${os.cpus().length}</li>`,
+      `<li>Modelo: ${os.cpus()[0].model}</li>`,
+      `<li>Frequência: ${os.cpus()[0].speed} Hz</li>`,
+    ].join('\r\n')}`;
 
-    getLocationInfos(clientIP, (
-      { city, postal_code: postalCode, region, country_name: country, company },
-    ) => {
+    getLocationInfos(clientIP, (locationData) => {
+      const {
+        city, postal_code: postalCode, region, country_name: country, company,
+      } = locationData;
       socket.write(startOfResponse);
       socket.write('<html><head><meta http-equiv="content-type" content="text/html;charset=utf-8">');
       socket.write('<title>Trybe 🚀</title></head><body>');
       socket.write('<H1>Explorando os Protocolos 🧐🔎</H1>');
       socket.write('<iframe src="https://giphy.com/embed/l3q2zVr6cu95nF6O4" width="480" height="236" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>');
-      socket.write(`<p data-testid="ip">${clientIP}</p>`);
-      socket.write(`<p data-testid="city">${city}</p>`);
-      socket.write(`<p data-testid="postal_code">${postalCode}</p>`);
-      socket.write(`<p data-testid="region">${region}</p>`);
-      socket.write(`<p data-testid="country">${country}</p>`);
-      socket.write(`<p data-testid="company">${company}</p>`);
-      socket.write(`<p data-testid="device">${device}</p>`);
-      socket.write(`<p data-testid="arch">${os.arch()}</p>`);
-      socket.write(`<p data-testid="cpu">${os.cpus().map(({ model, speed }) => `${model} ${speed}\r\n`)}</p>`);
-      socket.write(`<p data-testid="memory">${(os.totalmem() / 1024 / 1024 / 1024).toFixed(2)} GBs</p>`);
+      socket.write(`<p data-testid='ip'>IP: ${clientIP}</p>`);
+      socket.write(`<p data-testid='city'>Cidade: ${city}</p>`);
+      socket.write(`<p data-testid='postal_code'>CEP: ${postalCode}</p>`);
+      socket.write(`<p data-testid='region'>Estado: ${region}</p>`);
+      socket.write(`<p data-testid='country'>País: ${country}</p>`);
+      socket.write(`<p data-testid='company'>Fornecedora: ${company}</p>`);
+      socket.write(`<p data-testid='device'>Aquitetura: ${clientData}</p>`);
+      socket.write(`<p data-testid='arch'>${os.arch()}</p>`);
+      socket.write(`<p data-testid='cpu'>${cpuInfo}</p>`);
+      socket.write(`<p data-testid='memory'>RAM: ${os.totalmem()}</p>`);
       socket.write('</body></html>');
       socket.write(endOfResponse);
     });
--- src/location.js
+++ src/location.js
@@ -5,35 +5,16 @@ const options = {
   port: 443,
   path: '/',
   method: 'POST',
-  headers: {
-    'Content-Type': 'application/x-www-form-urlencoded',
-  },
+  headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
 };
 
 const getLocationInfos = (clientIP, cb) => {
-  // let city;
-  // let postalCode;
-  // let region;
-  // let country;
-  // let company;
-
   const req = https.request(options, (res) => {
     res.on('data', (locationDataRaw) => {
       const locationData = JSON.parse(locationDataRaw.toString());
+
       console.log('Location data:');
       console.log(locationData);
-
-      // console.log(`Cidade: ${city}`);
-      // console.log(`Código Postal (da cidade): ${postalCode}`);
-      // console.log(`Região e nome da região: ${region}`);
-      // console.log(`Nome do país: ${country}`);
-      // console.log(`Companhia (nome da provedora de internet): ${company}`);
-
-      // city = locationData.city;
-      // postalCode = locationData.postal_code;
-      // region = locationData.region;
-      // country = locationData.country_name;
-      // company = locationData.company;
 
       cb(locationData);
     });
@@ -43,7 +24,6 @@ const getLocationInfos = (clientIP, cb) => {
     console.error(e);
   });
 
-  // TO DO: Enviar mensagem (IP) ao server
   req.write(`ip=${clientIP}`);
 
   req.end();
